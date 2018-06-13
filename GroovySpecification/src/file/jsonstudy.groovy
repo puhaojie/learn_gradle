e19c52c144210af22dcc918bf8d9c74214235ee1
puhaojie <1293753897@qq.com>
2018-06-13 09:30:51 +0800

    修改Project执行外部命令

--- GroovySpecification/src/file/jsonstudy.groovy
+++ GroovySpecification/src/file/jsonstudy.groovy
@@ -50,7 +50,6 @@ println reponse.data.head.name
 def getNetworkData(String url) {
     //发送http请求
     def connection = new URL(url).openConnection()
-    connection.setRequestMethod('GET')
     connection.connect()
     def response = connection.content.text
     //将json转化为实体对象
